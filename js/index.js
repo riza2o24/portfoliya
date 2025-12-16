6e4ab5a90c0146f53b6406b323c34cc009bcbbbf
Otabek <abdullayevriza80@gmail.com>
2025-12-16 17:46:42 +0500

    wer

--- js/index.js
+++ js/index.js
@@ -4,7 +4,3 @@ let fname= prompt ("ismingizni kiriting ")
 
 confirm ("ismingiz" + (" ") + fname + (" ") + "mi")
 
-
-let qwName= prompt("ismingizni kiriting")
-
-console.log("ismi" + (" ") + qwName )
