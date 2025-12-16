440040dc705307ffb2a06a12c92a46693ee8cbc8
Otabek <abdullayevriza80@gmail.com>
2025-12-16 17:45:32 +0500

    wer

--- js/index.js
+++ js/index.js
@@ -3,3 +3,8 @@ alert ("Asallomu alaykum hush kelibsiz 😊")
 let fname= prompt ("ismingizni kiriting ")
 
 confirm ("ismingiz" + (" ") + fname + (" ") + "mi")
+
+
+let qwName= prompt("ismingizni kiriting")
+
+console.log("ismi" + (" ") + qwName )
